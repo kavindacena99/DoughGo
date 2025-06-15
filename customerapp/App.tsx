5b185ade0b185eeb9af2f38fd870bdad9d677b00
kavinda <kavindach4@gmail.com>
2025-06-15 13:27:14 +0530

    feat: implement navigation and add login/register screens

--- customerapp/App.tsx
+++ customerapp/App.tsx
@@ -1,16 +1,44 @@
 import "./global.css";
 import { View, StatusBar } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
+    
+    <NavigationContainer>
       <StatusBar 
         backgroundColor="orange"
         barStyle={"dark-content"}
         hidden={false}
       />
-      <HomeScreen />
-    </View>
+
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen}
+          options={{ headerShown: true, title: 'Login' }} 
+        />
+
+        <Stack.Screen 
+          name="Register" 
+          component={RegisterScreen} 
+          options={{ headerShown: true, title: 'Register' }}
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
