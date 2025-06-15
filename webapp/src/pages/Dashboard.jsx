ab6bfb4944f75c3223fd78033f9a034640bfb5e3
mbandara1 <120000451+NiroshanMadusanka@users.noreply.github.com>
2025-06-15 11:17:16 +0530

    dashboard frontend

--- webapp/src/pages/Dashboard.jsx
+++ webapp/src/pages/Dashboard.jsx
@@ -1,13 +1,46 @@
+import React from "react";
+import Navbar from "../components/Navbar";
 import LogoutButton from "../components/LogoutButton";
+import Footer from "../components/Footer";
+import "./Dashboard.css";
 
-function Dashboard(){
-    return(
-        <div>
-            <h1>Bakery Owner Dashboard</h1>
-            <LogoutButton />
+import { useNavigate } from "react-router-dom";
 
+function Dashboard() {
+  const navigate = useNavigate();
+
+  const handleAddVehicle = () => {
+    navigate("/addvehicle");
+  };
+
+  const handleAddItems = () => {
+    navigate("/additems");
+  };
+
+  const handleSelectRoutes = () => {
+    navigate("/selectroutes");
+  };
+
+  const handleLoadItems = () => {
+    navigate("/loaditems");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="dashboard-container">
+        <h1>Bakery Owner Dashboard</h1>
+        <div className="cards-container">
+          <div className="dashboard-card" onClick={handleAddVehicle}>Add Vehicle</div>
+          <div className="dashboard-card" onClick={handleAddItems}>Add Items</div>
+          <div className="dashboard-card" onClick={handleSelectRoutes}>Select Routes</div>
+          <div className="dashboard-card" onClick={handleLoadItems}>Load Items</div>
         </div>
-    );
+        <LogoutButton />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Dashboard;
